afa67f8dd66ce264470f4ef2eafbec4de80cd5f4
Amber Mirza <127652335+mirza-amber@users.noreply.github.com>
2026-09-07 23:34:28 +0530

    Set JSON request body size limit to 16kb

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,9 @@ const authRouter = require("./routes/auth.routes.js")
 const cors = require("cors")
 
 const app = express();
-app.use(express.json())
+app.use(express.json({
+    limit:"16kb"
+}))
 app.use(express.urlencoded({ extended: true })); // for reading multipart/form-data, can also use multer
 
 
